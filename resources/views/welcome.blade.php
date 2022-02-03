bbbd55e51cc42c40b03cc0f088c70937821badc9
FamosoRandom <ianis.grochowski@orange.fr>
2022-02-03 13:10:37 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-blue-500 h-screen">
+	<div class="w-full h-1/4 p-8 grid grid-cols-4">
+		<div class="bg-slate-100 w-1/2">
+			
+		</div>
+
+		<div class="bg-slate-100 w-1/2">
+			
+		</div>
+
+		<div class="bg-slate-100 w-1/2">
+			
+		</div>
+
+		<div class="bg-slate-100 w-1/2">
+			
+		</div>
+	</div>
+</body>
+</html>
