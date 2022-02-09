9571a13b1d35297ec2e5aa5105e79ac9e4cad78f
FamosoRandom <ianis.grochowski@orange.fr>
2022-02-09 13:10:26 +0100

    Added top navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,103 +42,108 @@
     </style>
 </head>
 <body class="bg-zinc-900 h-screen w-screen">
-<div class="carousel relative rounded relative overflow-hidden shadow-xl h-4/6">
-    <div class="carousel-inner relative overflow-hidden w-full h-full">
-        <!--Slide 1-->
-        <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden=""
-        checked="checked">
-        <div class="carousel-item absolute opacity-0 bg-center bg-cover h-full" style="background-image: url(https://mdbootstrap.com/img/new/slides/052.jpg)">
+    <div class="fixed container bg-gradient-to-r max-w-full w-full h-12 from-cyan-300 via-cyan-400 to-blue-600 z-50">
 
-        </div>
-        <label for="carousel-3"
-        class="control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto flex justify-center content-center"><i
-            class="fas fa-angle-left mt-3"></i></label>
-        <label for="carousel-2"
-        class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto"><i
-            class="fas fa-angle-right mt-3"></i></label>
+    </div>
+    
+    <div class="carousel relative rounded relative overflow-hidden shadow-xl h-3/6">
+        <div class="carousel-inner mt-14 relative overflow-hidden w-full h-5/6">
+            <!--Slide 1-->
+            <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden=""
+            checked="checked">
+            <div class="carousel-item absolute opacity-0 bg-center bg-cover h-full" style="background-image: url(https://mdbootstrap.com/img/new/slides/052.jpg)">
 
-        <!--Slide 2-->
-        <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-        <div class="carousel-item absolute opacity-0 bg-center bg-cover h-full" style="background-image: url(https://mdbootstrap.com/img/new/slides/043.jpg)">
-        </div>
-        <label for="carousel-1"
-        class=" control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto"><i
-            class="fas fa-angle-left mt-3"></i></label>
-        <label for="carousel-3"
-        class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto"><i
-            class="fas fa-angle-right mt-3"></i></label>
-
-        <!--Slide 3-->
-        <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-        <div class="carousel-item absolute opacity-0 bg-center bg-cover h-full" style="background-image: url(https://mdbootstrap.com/img/new/slides/054.jpg)">
-        </div>
-        <label for="carousel-2"
-        class="control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto"><i
-            class="fas fa-angle-left mt-3"></i></label>
-        <label for="carousel-1"
-        class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto"><i
-            class="fas fa-angle-right mt-3"></i></label>
-
-        <!-- Add additional indicators for each slide-->
-        <ol class="carousel-indicators">
-        <li class="inline-block mr-3">
-            <label for="carousel-1"
-            class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-        </li>
-        <li class="inline-block mr-3">
-            <label for="carousel-2"
-            class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-        </li>
-        <li class="inline-block mr-3">
+            </div>
             <label for="carousel-3"
-            class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-        </li>
-        </ol>
+            class="control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto flex justify-center content-center"><i
+                class="fas fa-angle-left mt-3"></i></label>
+            <label for="carousel-2"
+            class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto"><i
+                class="fas fa-angle-right mt-3"></i></label>
 
-    </div>
-</div>
-<div class="container absolute mx-auto px-2 inset-x-0 bottom-0 w-full lg:h-2/6">
-    <div class="flex overflow-x-auto -mx-1 h-full">
-        <!-- Column -->
-        <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+            <!--Slide 2-->
+            <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
+            <div class="carousel-item absolute opacity-0 bg-center bg-cover h-full" style="background-image: url(https://mdbootstrap.com/img/new/slides/043.jpg)">
+            </div>
+            <label for="carousel-1"
+            class=" control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto"><i
+                class="fas fa-angle-left mt-3"></i></label>
+            <label for="carousel-3"
+            class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto"><i
+                class="fas fa-angle-right mt-3"></i></label>
 
-            <!-- Article -->
-            <article class="overflow-hidden rounded-lg shadow-lg bg-neutral-50 h-full">
+            <!--Slide 3-->
+            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
+            <div class="carousel-item absolute opacity-0 bg-center bg-cover h-full" style="background-image: url(https://mdbootstrap.com/img/new/slides/054.jpg)">
+            </div>
+            <label for="carousel-2"
+            class="control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto"><i
+                class="fas fa-angle-left mt-3"></i></label>
+            <label for="carousel-1"
+            class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto"><i
+                class="fas fa-angle-right mt-3"></i></label>
 
-            </article>
-            <!-- END Article -->
+            <!-- Add additional indicators for each slide-->
+            <ol class="carousel-indicators">
+            <li class="inline-block mr-3">
+                <label for="carousel-1"
+                class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+            </li>
+            <li class="inline-block mr-3">
+                <label for="carousel-2"
+                class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+            </li>
+            <li class="inline-block mr-3">
+                <label for="carousel-3"
+                class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+            </li>
+            </ol>
 
         </div>
-        <!-- END Column -->
-		
-		<div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-			<article class="overflow-hidden rounded-lg shadow-lg bg-neutral-50 h-full">
-				
-			</article>
-		</div>
-		<div class="my-1 px-1 w-full flex-shrink-0 lg:w-1/4 lg:px-4 lg:my-4">
-			<article class="w-full h-full bg-neutral-50 rounded-lg">
-				
-			</article>
-		</div>
-		<div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 lg:h-50">
-			<article class="w-full h-full bg-neutral-50 rounded-lg">
-
-			</article>
-		</div>
-
-        <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 lg:h-50">
-			<article class="w-full h-full bg-neutral-50 rounded-lg">
-
-			</article>
-		</div>
-
-        <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 lg:h-50">
-			<article class="w-full h-full bg-neutral-50 rounded-lg">
-
-			</article>
-		</div>
     </div>
-</div>
+
+    <div class="container absolute mx-auto px-2 inset-x-0 bottom-0 w-full lg:h-2/5">
+        <div class="flex overflow-x-auto -mx-1 h-full">
+            <!-- Column -->
+            <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg bg-neutral-300 h-full">
+
+                </article>
+                <!-- END Article -->
+
+            </div>
+            <!-- END Column -->
+            
+            <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+                <article class="overflow-hidden rounded-lg shadow-lg bg-neutral-300 h-full">
+                    
+                </article>
+            </div>
+            <div class="my-1 px-1 w-full flex-shrink-0 lg:w-1/4 lg:px-4 lg:my-4">
+                <article class="w-full h-full bg-neutral-300 rounded-lg">
+                    
+                </article>
+            </div>
+            <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 lg:h-50">
+                <article class="w-full h-full bg-neutral-300 rounded-lg">
+
+                </article>
+            </div>
+
+            <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 lg:h-50">
+                <article class="w-full h-full bg-neutral-300 rounded-lg">
+
+                </article>
+            </div>
+
+            <div class="my-1 px-1 w-full flex-shrink-0 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 lg:h-50">
+                <article class="w-full h-full bg-neutral-300 rounded-lg">
+
+                </article>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
