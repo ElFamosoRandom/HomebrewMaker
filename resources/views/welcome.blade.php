493dc4979fb64606ee810ea0e283b1243aa1ba86
ianis grochowski <ianis>
2022-04-24 15:46:59 +0200

    Added title, buttons, wave and cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,13 +52,73 @@
     </style>
 </head>
 <body class="bg-zinc-400 overflow-hidden">
-    <div class="w-full h-screen bg-cover p-20" style="background-image: url('https://i.pinimg.com/originals/85/94/fd/8594fdb3396363a23030592a0a22088b.jpg')">
-        <div class=" border-solid border-4 border-sky-500 w-full h-full rounded-md" >
-            <div class="-m-1 -p-1 w-1/2 h-full">
-                <svg id="visual" class="w-full h-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><path d="M367 0L372.3 20C377.7 40 388.3 80 401.2 120C414 160 429 200 433.8 240C438.7 280 433.3 320 421 360C408.7 400 389.3 440 380.7 480C372 520 374 560 375 580L376 600L0 600L0 580C0 560 0 520 0 480C0 440 0 400 0 360C0 320 0 280 0 240C0 200 0 160 0 120C0 80 0 40 0 20L0 0Z" fill="#0ea4e9" stroke-linecap="round" stroke-linejoin="miter"></path></svg>
-            </div>
+    <div class="w-full h-screen bg-cover pl-4 pr-4 pt-4 pb-4" style="background-image: url('https://cdnb.artstation.com/p/assets/images/images/024/684/143/large/pixel-cat-2020-02-25-gai1.jpg?1583202914')">
+        <div class=" border-solid border-8 borer-slate-50 w-full h-full rounded-md" >
+          <div>
+            <button type="button" class='absolute right-10 top-10 w-36 h-12 bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-xs uppercase font-bold shadow-md rounded-full mx-auto'>
+              <div class="flex sm:flex-cols-12 gap-2">
+                  <div class="pl-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
+                      </svg>
+                  </div>
+                  <div class="col-span-2 pt-1">Button name</div>
+              </div>    
+            </button>
+            <button type="button" class='absolute right-52 top-10 w-36 h-12 bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-xs uppercase font-bold shadow-md rounded-full mx-auto'>
+              <div class="flex sm:flex-cols-12 gap-2">
+                  <div class="pl-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
+                      </svg>
+                  </div>
+                  <div class="col-span-2 pt-1">Button name</div>
+              </div>    
+            </button>
+          </div>
+          <div class="w-72 pt-4 pl-4">
+            <img style="" src="{{ asset('images/logo.png') }}" alt="tag">
+          </div>
         </div>
     </div>
+    <div style="height: 30%; width: 100%; overflow: hidden; position:absolute; top:70%; left:0;">
+      <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+        <path d="M-7.34,6.44 C114.55,110.05 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #f8fafc;">
+        </path>
+      </svg>
+    </div>
+    <div class="flex space-x-7 w-full pl-7">
+      <div class="relative -top-28 max-w-md h-72 py-4 px-8 bg-slate-200 shadow-lg rounded-lg z-30 item flex-auto">
+          <div class="flex justify-center -mt-16">
+            <img class="w-20 h-20" src="{{ asset('images/sword.png') }}">
+          </div>
+          <div>
+            <h2 class="text-gray-800 text-3xl font-semibold text-center">Design Tools</h2>
+          </div>
+      </div>
+      <div class="relative -top-28 max-w-md py-4 px-8 bg-slate-200 shadow-lg rounded-lg z-30 item flex-auto">
+          <div class="flex justify-center -mt-16">
+            <img class="w-20 h-20" src="{{ asset('images/gears.png') }}">
+          </div>
+          <div>
+            <h2 class="text-gray-800 text-3xl font-semibold text-center">Design Tools</h2>
+          </div>
+          
+      </div>
+      <div class="relative -top-28 max-w-md py-4 px-8 bg-slate-200 shadow-lg rounded-lg z-30 item flex-auto">
+          <div class="flex justify-center -mt-16">
+            <img class="w-20 h-20" src="{{ asset('images/mage.png') }}">
+          </div>
+          <div>
+            <h2 class="text-gray-800 text-3xl font-semibold text-center">Design Tools</h2>
+          </div>
+      </div>
+    <div>
+    <!--<div style="height: 50%; width: 25%; overflow: hidden; position:absolute; top:0; left:0;">
+      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+        <path fill="#FFFFFF" d="M36.2,-51.4C48.8,-48.2,62.3,-41.4,67.5,-30.7C72.6,-19.9,69.5,-5.2,66.9,9.3C64.3,23.8,62.3,38,53.2,43.1C44.1,48.2,27.9,44.1,15.3,44C2.8,43.9,-6.2,47.7,-18.2,49.8C-30.3,51.9,-45.4,52.1,-54.3,45.2C-63.3,38.2,-66.1,24.1,-65.9,10.8C-65.7,-2.4,-62.6,-14.8,-59.4,-29.4C-56.3,-44,-53.1,-60.9,-43.2,-65.3C-33.3,-69.8,-16.7,-61.8,-2.4,-58C11.8,-54.3,23.6,-54.6,36.2,-51.4Z" transform="translate(100 100)" />
+      </svg>
+    </div>-->
     
     <header style="" class="">
         
